3ead3824cc14383b57f8d674c0747ed5e51a23ae
owen <269315440@qq.com>
2016-06-01 22:55:39 +0800

    no message

---
+++ 手动添加Xib/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  手动添加Xib
+//
+//  Created by owen on 16/6/1.
+//  Copyright © 2016年 owen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 手动添加Xib/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  手动添加Xib
+//
+//  Created by owen on 16/6/1.
+//  Copyright © 2016年 owen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
